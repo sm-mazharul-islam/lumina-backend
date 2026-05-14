839cdda010dd2f5d9200bf29c8bb9b9867d51de7
sm-mazharul-islam <dev.sm.mazharul@gmail.com>
2026-05-14 11:57:40 +0600

    Update: Ai Controller

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import express from "express";
 import dotenv from "dotenv";
+import express from "express";
 import cors from "cors";
 import { connectDB } from "./config/db";
 import authRoutes from "./routes/authRoutes";
